351165b3c2e4152d2ab37f8588470b3e8a6be425
a <a>
2021-10-15 16:15:42 -0400

    messing with animations

--- radio.js
+++ radio.js
@@ -8,32 +8,62 @@ let content2 = document.getElementById("content2")
 let content3 = document.getElementById("content3")
 let content4 = document.getElementById("content4")
 
-radio1.onclick = (function() {
-    content1.style.display="block"
-    content2.style.display="none"
-    content3.style.display="none"
-    content4.style.display="none"
-})
 
-radio2.onclick = (function() {
-    content1.style.display="none"
-    content2.style.display="block"
-    content3.style.display="none"
-    content4.style.display="none"
-})
 
-radio3.onclick = (function() {
-    content1.style.display="none"
-    content2.style.display="none"
-    content3.style.display="block"
-    content4.style.display="none"
-})
+let currentContent = content1;
 
-radio4.onclick = (function() {
-    content1.style.display="none"
-    content2.style.display="none"
-    content3.style.display="none"
-    content4.style.display="block"
-})
+
+function setCurrentContent(content){
+    let otherContent = [content1, content2, content3, content4].filter(item => item !== content)
+    if (content != currentContent){
+        currentContent.classList.remove("radio-content-slidein")
+        currentContent.classList.add("radio-content-slideout")
+        currentContent = content;
+        setTimeout()
+        content.style.display="block"
+        content.classList.remove("radio-content-slideout")
+        content.classList.add("radio-content-slidein")
+    }
+}
+
+// radio1.onclick = function(){setCurrentContent(content1)};
+// radio2.onclick = function(){setCurrentContent(content2)};
+// radio3.onclick = function(){setCurrentContent(content3)};
+// radio4.onclick = function(){setCurrentContent(content4)};
+
+// radio1.onclick = (function() {
+//     content1.style.display="block"
+//     content1.classList.add("radio-content-slidein")
+//     content2.style.display="none"
+//     content3.style.display="none"
+//     content4.style.display="none"
+//     content2.classList.add
+//     content3.classList.add
+//     content4.classList.add
+// })
+
+// radio2.onclick = (function() {
+//     content1.style.display="none"
+//     content2.style.display="block"
+//     content2.classList.add("radio-content-slidein")
+//     content3.style.display="none"
+//     content4.style.display="none"
+// })
+
+// radio3.onclick = (function() {
+//     content1.style.display="none"
+//     content2.style.display="none"
+//     content3.style.display="block"
+//     content4.style.display="none"
+// })
+
+// radio4.onclick = (function() {
+//     content1.style.display="none"
+//     content2.style.display="none"
+//     content3.style.display="none"
+//     content4.style.display="block"
+// })
+
+radio1.click()
 
 
